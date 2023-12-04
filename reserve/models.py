5a21ae93fbea655c4e025e8839ae302ae7496b43
johnson-cc <313875761@qq.com>
2023-12-04 17:04:49 +0800

    version.kkl

--- reserve/models.py
+++ reserve/models.py
@@ -2,20 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth.models import User  # 引入Django内置的User模型
+
+
 # Create your models here.
 
 class Flight(models.Model):
-    #利用django中默认自增的id作为主键
-    flight_name = models.CharField(max_length=255)#班次，比如CZ8739，最大长度255。
-    departure = models.CharField(max_length=255)#起飞地点
-    destination = models.CharField(max_length=255)#目的地
-    departure_time = models.DateTimeField()#起飞时间
-    arrival_time = models.DateTimeField()#到达时间
-    airline = models.CharField(max_length=255)#航空公司
-    price = models.DecimalField(max_digits=10, decimal_places=2)#票价
+    # 利用django中默认自增的id作为主键
+    flight_name = models.CharField(max_length=255)  # 班次，比如CZ8739，最大长度255。
+    departure = models.CharField(max_length=255)  # 起飞地点
+    destination = models.CharField(max_length=255)  # 目的地
+    departure_time = models.DateTimeField()  # 起飞时间
+    arrival_time = models.DateTimeField()  # 到达时间
+    airline = models.CharField(max_length=255)  # 航空公司
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # 票价
     capacity = models.IntegerField(default=0, null=True)  # 座位总数
     book_sum = models.IntegerField(default=0, null=True)  # 订票总人数
     seat_info = models.TextField()  # 用TextField处理长文本字段
+    duration = models.CharField(max_length=10, blank=True)  # 添加持久化字段
+
+    def save(self, *args, **kwargs):
+        time_diff = self.arrival_time - self.departure_time  # 计算时间差
+        hours, remainder = divmod(time_diff.total_seconds(), 3600)  # 将秒数转换为小时和余下的秒数
+        minutes = remainder // 60  # 计算分钟
+        self.duration = f'{int(hours)}h-{int(minutes)}m'  # 格式化时间差并赋值给duration字段
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.flight_name
@@ -23,8 +33,8 @@ class Flight(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    balance = models.IntegerField() #用户余额
-    resident_num=models.CharField('resident identity card number', max_length=18)#身份证号
+    balance = models.IntegerField()  # 用户余额
+    resident_num = models.CharField('resident identity card number', max_length=18)  # 身份证号
     telephone = models.CharField('Telephone', max_length=11, blank=True)
 
     def __str__(self):
@@ -39,19 +49,20 @@ class Order(models.Model):
     ]
     # Django会自动为您创建一个名为id的自增主键字段
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 直接使用User模型
-    flight = models.ForeignKey('Flight', on_delete=models.CASCADE)#航班id
-    booking_time = models.DateTimeField()#订票时间
-    order_status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES)#订单状态
+    flight = models.ForeignKey('Flight', on_delete=models.CASCADE)  # 航班id
+    booking_time = models.DateTimeField()  # 订票时间
+    order_status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES)  # 订单状态
 
     def __str__(self):
         return f"Order {self.id}"  # 使用默认的id字段
 
+
 class FinancialReport(models.Model):
-    report_id = models.AutoField(primary_key=True)#报告的唯一id
-    creation_time = models.DateTimeField(auto_now_add=True)#创建时间
-    total_revenue = models.DecimalField(max_digits=10, decimal_places=2)#总收入
-    total_expenditure = models.DecimalField(max_digits=10, decimal_places=2)#总支出
-    profit = models.DecimalField(max_digits=10, decimal_places=2)#利润
+    report_id = models.AutoField(primary_key=True)  # 报告的唯一id
+    creation_time = models.DateTimeField(auto_now_add=True)  # 创建时间
+    total_revenue = models.DecimalField(max_digits=10, decimal_places=2)  # 总收入
+    total_expenditure = models.DecimalField(max_digits=10, decimal_places=2)  # 总支出
+    profit = models.DecimalField(max_digits=10, decimal_places=2)  # 利润
 
     def __str__(self):
         return f"Financial Report {self.report_id}"
--- reserve/views.py
+++ reserve/views.py
@@ -3,45 +3,168 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
-from .models import Flight,Order
+from .models import Flight, Order
 from django.utils import timezone
 from .models import UserProfile
 from django.utils.dateparse import parse_date
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.db.models import Sum
-from datetime import timedelta
-
+from datetime import timedelta, datetime
+from operator import attrgetter
+from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'index.html')
 
 
+# def search(request):
+#     print("hello_world")
+#     if request.method == 'POST':
+#         departure_city = request.POST.get('departure_city')
+#         arrival_city = request.POST.get('arrival_city')
+#         departure_date_str = request.POST.get('departure_date')
+#
+#         if not departure_city or not arrival_city or not departure_date_str:
+#             messages.warning(request, '请填写所有字段。')
+#             return redirect('index')
+#
+#         departure_date = parse_date(departure_date_str)
+#
+#         flights = Flight.objects.filter(
+#             departure=departure_city,
+#             destination=arrival_city,
+#             departure_time__date=departure_date
+#         ).order_by('departure_time')
+#         print({
+#             'departure_city': departure_city,
+#             'arrival_city': arrival_city,
+#             'departure_date': departure_date,
+#             'flights': flights})
+#         return render(request, 'search.html', {
+#             'departure_city': departure_city,
+#             'arrival_city': arrival_city,
+#             'departure_date': departure_date,
+#             'flights': flights})
+#     else:
+#         return render(request, 'search.html')
+
+
 def search(request):
     if request.method == 'POST':
+        button_clicked = request.POST.get('button_clicked')
+        last_button_clicked = request.session.get('last_button_clicked', 'departure_time_asc')
+
         departure_city = request.POST.get('departure_city')
         arrival_city = request.POST.get('arrival_city')
         departure_date_str = request.POST.get('departure_date')
 
-        # 将字符串格式的日期转换为日期对象
+        if not departure_city or not arrival_city or not departure_date_str:
+            messages.warning(request, '请填写所有字段。')
+            return redirect('index')
+
         departure_date = parse_date(departure_date_str)
-        #print(departure_date)
-        # 在查询中只比较日期部分
+
+        request.session['departure_city'] = departure_city
+        request.session['arrival_city'] = arrival_city
+        request.session['departure_date'] = departure_date_str
+
+        flights = Flight.objects.filter(
+            departure=departure_city,
+            destination=arrival_city,
+            departure_time__date=departure_date
+        ).order_by('departure_time')
+        for flight in flights:
+            flight.save()
+
+
+        if button_clicked != last_button_clicked:
+            request.session['last_button_clicked'] = button_clicked
+
+        if button_clicked == '起飞时间早-晚' and last_button_clicked != '起飞时间早-晚':
+            flights = flights.order_by('departure_time')
+            button_highlight = '起飞时间早-晚'
+        elif button_clicked == '到达时间早-晚' and last_button_clicked != '到达时间早-晚':
+            flights = flights.order_by('arrival_time')
+            button_highlight = '到达时间早-晚'
+        elif button_clicked == '低价优先' and last_button_clicked != '低价优先':
+            flights = flights.order_by('price')
+            button_highlight = '低价优先'
+        elif button_clicked == '耗时短优先' and last_button_clicked != '耗时短优先':
+            flights = flights.order_by('duration')
+            button_highlight = '耗时短优先'
+        else:
+            button_highlight = last_button_clicked
+
+        return render(request, 'search.html', {
+            'flights': flights,
+            'departure_city': departure_city,
+            'arrival_city': arrival_city,
+            'departure_date': departure_date,
+            'button_highlight': button_highlight
+        })
+
+    else:
+        departure_city = request.session.get('departure_city', '')
+        arrival_city = request.session.get('arrival_city', '')
+        departure_date_str = request.session.get('departure_date', '')
+
+        if not departure_city or not arrival_city or not departure_date_str:
+            return render(request, 'search.html')
+
+        departure_date = parse_date(departure_date_str)
+
         flights = Flight.objects.filter(
             departure=departure_city,
             destination=arrival_city,
             departure_time__date=departure_date
         )
-        #print("QuerySet:", flights.query)  # 打印 SQL 查询
 
-        return render(request, 'search.html', {'flights': flights})
+        last_button_clicked = request.session.get('last_button_clicked', '起飞时间早-晚')
 
-    # 如果不是 POST 请求，可以重定向到首页或者显示错误消息
-    return redirect('index')
+        if last_button_clicked == '起飞时间早-晚':
+            flights = flights.order_by('departure_time')
+        elif last_button_clicked == '到达时间早-晚':
+            flights = flights.order_by('arrival_time')
+        elif last_button_clicked == '低价优先':
+            flights = flights.order_by('price')
+        elif last_button_clicked == '耗时短优先':
+            flights = flights.order_by('duration')
 
+        return render(request, 'search.html', {
+            'departure_city': departure_city,
+            'arrival_city': arrival_city,
+            'departure_date': departure_date,
+            'flights': flights,
+            'button_highlight': last_button_clicked
+        })
+
+
+# def search(request):
+#     if request.method == 'POST':
+#         departure_city = request.POST.get('departure_city')
+#         arrival_city = request.POST.get('arrival_city')
+#         departure_date_str = request.POST.get('departure_date')
+#
+#         # 将字符串格式的日期转换为日期对象
+#         departure_date = parse_date(departure_date_str)
+#         #print(departure_date)
+#         # 在查询中只比较日期部分
+#         flights = Flight.objects.filter(
+#             departure=departure_city,
+#             destination=arrival_city,
+#             departure_time__date=departure_date
+#         )
+#         #print("QuerySet:", flights.query)  # 打印 SQL 查询
+#
+#         return render(request, 'search.html', {'flights': flights})
+#
+#     # 如果不是 POST 请求，可以重定向到首页或者显示错误消息
+#     return redirect('index')
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
@@ -52,28 +175,35 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
 
+
 '''    用户登录'''
+
+
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = auth.authenticate(username=username, password=password)
         if user:
-            auth.login(request, user)  #这里做了登录
+            auth.login(request, user)  # 这里做了登录
             if user.is_superuser:
                 return redirect('finance')
             else:
                 return redirect('userprofile')  # 跳转至首页
     return render(request, "login.html")
 
+
 @login_required(login_url='/login/')
 def userprofile(request):
-    orders = Order.objects.filter(user=request.user,order_status='成功')
+    orders = Order.objects.filter(user=request.user, order_status='成功')
     return render(request, 'userprofile.html', {'orders': orders})
+
 
 def logout(request):
     auth.logout(request)
     return redirect('login')  # 退出后，页面跳转至登录界面
+
+
 @login_required(login_url='/login/')
 def finance(request):
     # 获取所有订单
@@ -101,7 +231,7 @@ def finance(request):
     yearly_flights, yearly_income, yearly_refunds, yearly_profit = calculate_financial_data(year_ago)
 
     return render(request, 'finance.html', {
-        'orders':orders,
+        'orders': orders,
         'weekly_flights': weekly_flights,
         'weekly_income': weekly_income,
         'weekly_refunds': weekly_refunds,
@@ -161,9 +291,10 @@ def book_ticket(request, flight_id):
         # 重定向到用户个人资料页面
         return redirect('userprofile')
 
-    return render(request, 'book_ticket.html', {'flight': flight,'remaining_seats': remaining_seats})
+    return render(request, 'book_ticket.html', {'flight': flight, 'remaining_seats': remaining_seats})
 
-def cancel_order(request,order_id):
+
+def cancel_order(request, order_id):
     # 缺少检查：只有起飞时间大于当前时间才能退票
     # 获取订单对象，确保只有订单的所有者才能退票
     order = get_object_or_404(Order, pk=order_id, user=request.user)
